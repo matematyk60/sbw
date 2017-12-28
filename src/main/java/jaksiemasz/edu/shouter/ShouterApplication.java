6bf9e8e6c2e974611e0174ccf9573e11b4e6fae2
matematyk60 <dawid.gdk@gmail.com>
2017-12-28 17:41:37 +0100

    init commit

---
+++ src/main/java/jaksiemasz/edu/shouter/ShouterApplication.java
@@ -0,0 +1,12 @@
+package jaksiemasz.edu.shouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShouterApplication.class, args);
+	}
+}
